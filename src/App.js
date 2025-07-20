16b59091ff5283983494c1f7f48a078708292efc
srduran10 <sr.duran10@gmail.com>
2025-07-20 16:14:49 -0400

    Create App.js
    Crear src/App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>🚀 Bot my Wallet está vivo</h1>
+      <p>Tu asistente financiero ya está en camino</p>
+    </div>
+  );
+}
+
+export default App;

cf36e2b1c057287186be8eca95c5c883f5a2d2ae
srduran10 <sr.duran10@gmail.com>
2025-07-20 17:24:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { PortfolioProvider } from './context/PortfolioContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <PortfolioProvider>
+    <App />
+  </PortfolioProvider>
+);
